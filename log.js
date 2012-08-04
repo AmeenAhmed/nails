7cb7a3464a1f1f59d8f246247f2f4d50f5e6dbb0
Ameen Ahmed <ameen.ahmed.b@gmail.com>
2012-08-05 05:14:03 +0530

    added log.js

--- log.js
+++ log.js
@@ -1,3 +1,27 @@
-exports.log = function() {
-	
+//@ Author : Ameen Ahmed
+// Log.js : 
+// Log helper with info, debug, error messages.
+
+function printLogTime() {
+	var date = new Date();
+	var month = prefixZero(date.getMonth());
+	var dateNum = prefixZero(date.getDate());
+	var hours = prefixZero(date.getHours());
+	var minutes = prefixZero(date.getMinutes());
+	var seconds = prefixZero(date.getSeconds());
+
+	return "["+ date.getFullYear()+"-"+month+"-"+dateNum+" "+
+				hours+":"+minutes+":"+seconds + "] "
+				+ "INFO  ";
+}
+
+function prefixZero(num) {
+	if(num < 10) {
+		return "0" + num;
+	}
+	return num;
+}
+
+exports.info = function(message) {
+	console.log(printLogTime() + message);
 }
